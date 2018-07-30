16b198087b9038514b40b50fc05ffe6968497c98
Subin Siby <subins2000@gmail.com>
2018-07-30 20:48:19 +0530

    Enable commands

--- server.php
+++ server.php
@@ -1,4 +1,17 @@
 <?php
+if ($_SERVER['REQUEST_URI'] === '/request' && isset($_POST['cmd'])) {
+    file_put_contents(__DIR__ . '/request', json_encode([
+        'cmd'    => $_POST['cmd'],
+        'values' => isset($_POST['values']) ? $_POST['values'] : [],
+    ]));
+    exit;
+}
+
+if ($_SERVER['REQUEST_URI'] === '/response') {
+    echo file_get_contents(__DIR__ . '/response');
+    exit;
+}
+
 $live_response =  json_decode(@file_get_contents(__DIR__ . '/live_response'), true);
 
 if (empty($live_response))
@@ -36,13 +49,31 @@ $live_response['likes'] = array_reverse($live_response['likes']);
                 <div class="col-6">
                     <h2>Commands</h2>
                     <button type="button"class="btn btn-danger" id="end_stream">End Stream</button>
-                    <button type="button"class="btn btn-info" id="get_info">Info</button>
-                    <button type="button"class="btn btn-warning" id="get_info">Clear Likes & Comments</button>
-                    <div>
-                        <blockquote class="blockquote" id="response" style="height: 100px;">
+                    <button type="button"class="btn btn-info" id="get_info" data-cmd="info">Info</button>
+                    <button type="button"class="btn btn-warning" id="clear" data-cmd="clear">Clear Likes & Comments</button>
+                    <div style="margin-top: 20px;">
+                        <blockquote class="blockquote border" style="height: 100px;">
+                            <div id="response"></div>
                             <footer class="blockquote-footer">Response</footer>
                         </blockquote>
                     </div>
+                    <script>
+                        $(function() {
+                            $('#end_stream').on('click', function() {
+                                var keep = prompt('Would you like to keep the stream archived for 24 hours ? Type "yes" to keep or anything else to not archive');
+
+                                $.post('/request', {cmd: 'end', values: [keep]});
+                            });
+
+                            $('#get_info, #clear').on('click', function() {
+                                $.post('/request', {cmd: $(this).data('cmd')});
+                            });
+
+                            setInterval(function() {
+                                $('#response').load('/response');
+                            }, 1000);
+                        });
+                    </script>
                 </div>
             </div>
             <script>
