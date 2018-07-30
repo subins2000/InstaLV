e8003173d8ee8723ad3990da9ff4eddbccc76dc1
Subin Siby <subins2000@gmail.com>
2018-07-30 16:11:37 +0530

    Server handler

---
+++ server.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            
+        </div>
+    </body>
+</html>
