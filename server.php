cad00314163152225923f604f1af36951e7ac353
Subin Siby <subins2000@gmail.com>
2018-07-30 17:42:06 +0530

    add likes

--- server.php
+++ server.php
@@ -13,9 +13,28 @@ if (empty($live_response))
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     </head>
     <body>
+        <nav class="navbar navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">
+                InstaLiveStream!
+            </a>
+        </nav>
         <div class="container">
             <div class="row">
-                <div class="col-3">
+                <div class="col-6">
+                    <h2>Likes</h2>
+                    <ul class="list-group">
+                        <?php
+                        foreach ($live_response['likes'] as $username) {
+                            echo <<<HTML
+<li class="list-group-item">
+    {$username} liked
+</li>
+HTML;
+                        }
+                        ?>
+                    </ul>
+                </div>
+                <div class="col-6">
                     <h2>Comments</h2>
                     <ul class="list-group">
                         <?php
