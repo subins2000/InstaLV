310a3c80d2acada62a093a1357621a86a38dd7a2
Subin Siby <subins2000@gmail.com>
2018-07-30 19:39:19 +0530

    Added callbacks

--- config.php
+++ config.php
@@ -1,3 +1,13 @@
 <?php
 define('IG_USERNAME', 'USERNAME');
-define('IG_PASS', 'PASSWORD');
+define('IG_PASS', 'USERNAME');
+
+$cfg_callbacks = [
+    'like' => function($user) {
+        var_dump($user->getUsername());
+    },
+    'comment' => function($user, $comment) {
+        var_dump($user->getUsername());
+        var_dump($comment);
+    }
+];
