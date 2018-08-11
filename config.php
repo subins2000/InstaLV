ce4c9cec2179adf114911a00798ffb5211c55060
Subin Siby <subins2000@gmail.com>
2018-08-11 22:54:53 +0530

    Add callback config

--- config.php
+++ config.php
@@ -1,3 +1,14 @@
 <?php
 define('IG_USERNAME', 'USERNAME');
 define('IG_PASS', 'PASSWORD');
+
+$cfg_callbacks = [
+    'like' => function($user) {
+        $msg = $user->getUsername() . ' liked !';
+        var_dump($msg);
+    },
+    'comment' => function($user, $comment) {
+        $msg = $user->getUsername() . ' : ' . $comment;
+        var_dump($msg);
+    }
+];
