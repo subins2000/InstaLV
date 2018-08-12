7274eac9fdd2b644e2a9d23b5fff4a28e55db3a9
Subin Siby <subins2000@gmail.com>
2018-08-12 23:55:12 +0530

    Add example to show nitifications in GNU/Linux

--- config.php
+++ config.php
@@ -6,9 +6,17 @@ $cfg_callbacks = [
     'like' => function($user) {
         $msg = $user->getUsername() . ' liked !';
         var_dump($msg);
+        /**
+         * Show system notification in GNU/Linux OS
+         * exec('nohup notify-send "' . $msg . '" > /dev/null &');
+         */
     },
     'comment' => function($user, $comment) {
         $msg = $user->getUsername() . ' : ' . $comment;
         var_dump($msg);
+        /**
+         * Show system notification in GNU/Linux OS
+         * exec('nohup notify-send "' . $msg . '" > /dev/null &');
+         */
     }
 ];
